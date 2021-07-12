e6a8bbfe6032c023bc6439ca842fe89f93e74b7b
chj <chj1218@163.com>
2021-07-12 22:53:00 +0800

    auth-role-admin

---
+++ mycrowdfunding01-admin-parent/mycrowdfunding03-admin-component/src/main/java/org/study/crowd/service/api/AuthService.java
@@ -0,0 +1,16 @@
+package org.study.crowd.service.api;
+
+import org.study.crowd.entity.Auth;
+
+import java.util.List;
+import java.util.Map;
+
+public interface AuthService {
+    List<Auth> getAll();
+
+    List<Integer> getAssignedAuthIdByRoleId(Integer roleId);
+
+    void saveRoleAuthRelathinship(Map<String, List<Integer>> map);
+
+    List<String> getAssignedAuthNameByAdminId(Integer adminId);
+}
